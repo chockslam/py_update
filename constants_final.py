246452e01cece90ee1ec766757ea5bd6277bcb28
chockslam <64290122+chockslam@users.noreply.github.com>
2022-03-18 11:26:02 +0000

    first commit

---
+++ constants_final.py
@@ -0,0 +1,13 @@
+# Files used
+FILENAME_JSON = ''   # .json file to 
+FILENAME_CSV = 'SQL'                        # CSV file to be written and send.
+EXTENSION_CSV = '.csv'                 # json extension
+
+# Array of folders on a remote server
+FTP_PATH = ['clickandbuilds', 'Bcausam', 'wp-content', 'themes', 'astra']
+
+# SFTP account credentials
+FTP_HOST = 'access834021495.webspace-data.io'
+FTP_USERNAME = 'u101433123'
+FTP_PASSWORD = ''                       # note that storing password in the text is not safe
+FTP_PORT = 22
